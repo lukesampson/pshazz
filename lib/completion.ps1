c04f55e3506f358283a4c709eeb039a8a45f79ef
Luke Sampson <me@lukesampson.com>
2013-08-03 16:12:12 +1000

    fix tab after git (no space) replaces git with 'add'

--- lib/completion.ps1
+++ lib/completion.ps1
@@ -11,7 +11,7 @@ function global:tabExpansion($line, $lastWord) {
     $expression = [regex]::split($line, '[|;]')[-1].trimstart()
 
     foreach($cmd in $global:completions.keys) {
-        if($expression -match "^$cmd\s*(?<fragment>.*)") {
+        if($expression -match "^$cmd\s+(?<fragment>.*)") {
             return & $global:completions[$cmd] $matches['fragment']
         }
     }
