ae7c3f186cf71c6228e9fa537dc282097ebf83b3
Luke Sampson <me@lukesampson.com>
2013-08-15 22:06:16 +1000

    add install script

---
+++ bin/install.ps1
@@ -0,0 +1,12 @@
+if(!(test-path $profile)) {
+	$profile_dir = split-path $profile
+	if(!(test-path $profile_dir)) { mkdir $profiledir > $null } 
+	'' > $profile
+}
+if((gc $profile | sls 'pshazz') -eq $null) {
+	write-host 'adding pshazz to your powershell profile'
+	(gc $profile -raw) + \"`r`ntry { `$null = gcm pshazz -ea stop; pshazz init 'default' } catch { }`r`n\" > $profile
+} else {
+	write-host 'it looks like pshazz is already in your powershell profile, skipping'
+}
+pshazz init 'default'
