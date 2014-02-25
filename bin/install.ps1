9707cca03fa5adbfb4805f52fc59c5cae90a7189
Luke Sampson <me@lukesampson.com>
2014-02-25 14:55:22 +1000

    update pre-existing pshazz init in C:\Users\luke\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1

--- bin/install.ps1
+++ bin/install.ps1
@@ -3,12 +3,21 @@ if(!(test-path $profile)) {
 	if(!(test-path $profile_dir)) { mkdir $profile_dir > $null } 
 	'' > $profile
 }
-if((gc $profile | sls 'pshazz') -eq $null) {
+
+$old_init = "try { `$null = gcm pshazz -ea stop; pshazz init 'default' } catch { }"
+$new_init = "try { `$null = gcm pshazz -ea stop; pshazz init } catch { }"
+
+$text = gc $profile
+if(($text | sls 'pshazz') -eq $null) {
 	write-host 'adding pshazz to your powershell profile'
 
 	# read and write whole profile to avoid problems with line endings and encodings
-	$new_profile = @(gc $profile) + "try { `$null = gcm pshazz -ea stop; pshazz init 'default' } catch { }"
-    $new_profile > $profile
+	$new_profile = @($text) + "try { `$null = gcm pshazz -ea stop; pshazz init 'default' } catch { }"
+	$new_profile > $profile
+} elseif($text -contains $old_init) {
+	write-host 'updating pshazz init in your powershell profile'
+	$new_profile = $text -replace [regex]::escape($old_init), $new_init
+	$new_profile > $profile
 } else {
 	write-host 'it looks like pshazz is already in your powershell profile, skipping'
 }
