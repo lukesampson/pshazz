2d1955040e8f645cb430c334e1a4f3fe409648a0
Joe Ipson <joeipson@gmail.com>
2015-09-11 22:43:51 -0600

    Added number of insertions and deletions for git. Added new global variables for time and 'rightarrow' character. Added Powershell version of agnoster zsh theme.

--- plugins/git.ps1
+++ plugins/git.ps1
@@ -23,11 +23,39 @@ function global:pshazz:git:prompt {
 	if($ref) {
 		$vars.git_lbracket = $global:pshazz.git.prompt_lbracket
 		$vars.git_rbracket = $global:pshazz.git.prompt_rbracket
+		$vars.yes_git = ([char]0xe0b0);
 
 		$vars.git_branch = $ref -replace '^refs/heads/', '' # branch name
 		try { $status = git status --porcelain } catch { }
 		if($status) {
-			$vars.git_dirty = $global:pshazz.git.prompt_dirty
+			$vars.git_dirty = $global:pshazz.git.prompt_dirty;
+
+			try { $lines = git diff HEAD --shortstat } catch { }
+			if($lines) {
+				try { $individual_lines = $lines.Split("{,}") } catch { }
+				if($individual_lines) {
+					if($individual_lines.Count.Equals(2)) {
+						if($individual_lines[1] -match 'insertions') {
+							$insertions = $individual_lines[1].Trim().Split("{ }");
+							$vars.git_insertions = "+" + $insertions[0];
+						} else {
+							if($individual_lines[1] -match 'deletions') {
+								$deletions = $individual_lines[1].Trim().Split("{ }");
+								$vars.git_deletions = "-" + $deletions[0]
+							}
+						} 
+					} else {
+						if($individual_lines.Count.Equals(3)) {
+							$insertions = $individual_lines[1].Trim().Split("{ }");
+							$vars.git_insertions = "+" + $insertions[0];
+							$deletions = $individual_lines[2].Trim().Split("{ }");
+							$vars.git_deletions = "-" + $deletions[0]
+						}
+					}
+				}
+			}
 		}
+	} else {
+		$vars.no_git = ([char]0xe0b0);
 	}
 }
