9759f1ad851853ca9a63cf3b7d106f3671136e86
Luke Sampson <me@lukesampson.com>
2019-01-31 07:48:12 +1100

    Revert "Add random theme functionality"

--- libexec/pshazz-use.ps1
+++ libexec/pshazz-use.ps1
@@ -1,13 +1,9 @@
-# Usage: pshazz use <theme>
+﻿# Usage: pshazz use <theme>
 # Summary: Change the current theme
 # Help: This command will configure pshazz to use the specified theme.
 #
 # To revert to the default theme, use 'default'. E.g.:
 #     pshazz use default
-#
-# To use a random theme for each session, use 'random'. E.g.:
-#     pshazz use random
-
 param($name)
 
 . "$psscriptroot\..\lib\core.ps1"
@@ -20,11 +16,9 @@ if(!$name) {
 	exit 1
 }
 
-if ($theme -ne 'random') {
-	# make sure valid theme
-	$theme = theme $name
-	if(!$theme) { "pshazz: couldn't find the theme named '$name'"; exit 1 }
-}
+# make sure valid theme
+$theme = theme $name
+if(!$theme) { "pshazz: couldn't find the theme named '$name'"; exit 1 }
 
 # save theme
 set_config 'theme' $name
