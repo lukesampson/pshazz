cdb51b8e5f79ed7e34b61702561516559cf78f8b
Kied T L <eli.georgios@gmail.com>
2019-01-30 13:22:48 -0500

    Merged random and use commands

--- libexec/pshazz-use.ps1
+++ libexec/pshazz-use.ps1
@@ -1,4 +1,4 @@
-﻿# Usage: pshazz use <theme>
+# Usage: pshazz use <theme>
 # Summary: Change the current theme
 # Help: This command will configure pshazz to use the specified theme.
 #
@@ -16,9 +16,11 @@ if(!$name) {
 	exit 1
 }
 
-# make sure valid theme
-$theme = theme $name
-if(!$theme) { "pshazz: couldn't find the theme named '$name'"; exit 1 }
+if ($theme -ne 'random') {
+	# make sure valid theme
+	$theme = theme $name
+	if(!$theme) { "pshazz: couldn't find the theme named '$name'"; exit 1 }
+}
 
 # save theme
 set_config 'theme' $name
