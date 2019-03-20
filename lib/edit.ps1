2f66e0506448401d9f0dd8c38346ec525d8b6118
h404bi <h404bi@gmail.com>
2019-03-20 20:17:34 +0800

    refactor: format lib code

--- lib/edit.ps1
+++ lib/edit.ps1
@@ -1,14 +1,20 @@
-﻿$known_editors = "vim", "nano", "notepad2", "notepad++", "notepad"
+$known_editors = "vim", "nano", "notepad2", "notepad++", "notepad"
 
 function editor {
-	$editor = get_config 'editor'
-	if($editor) { return $editor }
-	foreach($editor in $known_editors) {
-		if(has_editor $editor) { return $editor }
-	}
-	return $null
+    $editor = get_config 'editor'
+    if ($editor) {
+        return $editor
+    }
+
+    foreach($editor in $known_editors) {
+        if (has_editor $editor) {
+            return $editor
+        }
+    }
+
+    return $null
 }
 
 function has_editor($name) {
-	try { gcm $name -ea stop; $true } catch { $false }
+    try { Get-Command $name -ErrorAction Stop; $true } catch { $false }
 }
