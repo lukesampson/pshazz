2868f1fe4acb5dac91790c4d47a398f4cadaf34b
Kiëd Llaentenn <32681240+kiedtl@users.noreply.github.com>
2019-03-20 11:01:33 -0400

    Change $env:USERPROFILE to $HOME in plugin.ps1

--- lib/plugin.ps1
+++ lib/plugin.ps1
@@ -1,12 +1,12 @@
 $plugindir = fullpath "$PSScriptRoot\..\plugins"
-$user_plugindir = $env:PSHAZZ_PLUGINS, "$env:USERPROFILE\pshazz\plugins" | Select-Object -first 1
+$user_plugindir = $env:PSHAZZ_PLUGINS, "${HOME}/pshazz/plugins" | Select-Object -first 1
 
 function plugin:init($name) {
     # try user plugin dir first
-    $path = "$user_plugindir\$name.ps1"
+    $path = "$user_plugindir/$name.ps1"
     if (!(Test-Path $path)) {
         # fallback to defaults
-        $path = "$plugindir\$name.ps1"
+        $path = "$plugindir/$name.ps1"
         if (!(Test-Path $path)) {
             Write-Warning "Couldn't find pshazz plugin '$name'."
             return $false
