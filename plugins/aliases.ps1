da17de9d45a0cc00baac1a24a703feb78e6764d7
David <Deide@live.co.uk>
2019-05-24 06:28:38 +0100

    fix(plugin): Fix aliases plugin (#98)

--- plugins/aliases.ps1
+++ plugins/aliases.ps1
@@ -1,53 +1,51 @@
 ﻿# fixes some insane powershell aliases that interfere with real programs
 # also adds some commonly used aliases
 # you can specify more aliases to add and remove in the theme under aliases.rm and aliases.add
-$remove = 'curl', 'wget', 'r'
-$add = @{
-	ll = 'ls';
-}
-
 function pshazz:aliases:init {
-	$remove = @($global:pshazz.theme.aliases.rm) + $remove |? { $_ } # theme overrides
+    # remove default/theme aliases
+    $remove = @($global:pshazz.theme.aliases.rm) + ('curl', 'wget', 'r') | Where-Object { $_ } # theme overrides
+    $remove | ForEach-Object {
+        # may need to execute the rm many times in parent scopes until really removed
+        # (set-alias -option allscope copies the alias to child scopes)
+        while (test-path "alias:$_") {
+            Remove-Item "alias:\$_" -Force
+        }
+    }
 
-	$remove |% {
-		# may need to execute the rm many times in parent scopes until really removed
-		# (set-alias -option allscope copies the alias to child scopes)
-		while(test-path "alias:$_") {
-			Remove-Item "alias:\$_" -force
-		}
-	}
+    # Add default aliases
+    Set-PAlias 'll' 'ls'
 
-	$global:pshazz.theme.aliases.add.keys |? { $_ } |% {
-		$add.$_ = $global:pshazz.theme.aliases.add.$_
-	}
-
-	$add.keys |% {
-		$alias = $_
-		$cmd = $add.$alias
-		if(($alias -match '\(') -or ($cmd -match ' ')) {
-			add_alias_with_params $alias $cmd # with params
-		} else {
-			set-alias $alias $cmd -opt allscope -scope global # without params
-		}
-	}
+    # Theme aliases
+    $global:pshazz.theme.aliases.add.psobject.Properties | ForEach-Object {
+        Set-PAlias $_.Name $_.Value
+    }
 }
 
-function add_alias_with_params($alias, $cmd) {
-	# alias with extra parameters, based on
-	# http://huddledmasses.org/powershell-power-user-tips-bash-style-alias-command/
-	$m = $alias | sls '([^\(]+)(\([^\)]+\))' | select -first 1
-	$in_param = $null
+function Set-PAlias($alias, $cmd) {
+    if (($alias -match '\(') -or ($cmd -match ' ')) {
+        Set-InterpolatedPAlias $alias $cmd # with params
+    }
+    else {
+        Set-Alias $alias $cmd -opt allscope -scope global # without params
+    }
+}
 
-	if($m) {
-		# has input parameters
-		$alias, $in_param = $m.matches.groups[1..2] |% { $_.value }
-	}
+function Set-InterpolatedPAlias($alias, $cmd) {
+    # alias with extra parameters, based on
+    # http://huddledmasses.org/powershell-power-user-tips-bash-style-alias-command/
+    $m = $alias | Select-String '([^\(]+)(\([^\)]+\))' | Select-Object -First 1
+    $in_param = $null
 
-	$fn_body = $cmd
-	if($in_param) {
-		$fn_body = "param$in_param $fn_body"
-	}
+    if ($m) {
+        # has input parameters
+        $alias, $in_param = $m.Matches.Groups[1..2] | ForEach-Object { $_.Value }
+    }
 
-	$null = new-item -path function: -name "global:pshazz.alias_$alias" -options allscope -value $fn_body -force
-	set-alias $alias "pshazz.alias_$alias" -opt allscope -scope global
+    $fn_body = $cmd
+    if ($in_param) {
+        $fn_body = "param$in_param $fn_body"
+    }
+
+    $null = New-Item -Path function: -Name "global:pshazz.alias_$alias" -Options allscope -Value $fn_body -Force
+    Set-Alias $alias "pshazz.alias_$alias" -Opt allscope -Scope global
 }
