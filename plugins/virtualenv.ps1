ddfe6f392c540b5bc7c319270bcf94ae1707b9b3
Chawye Hsu <h404bi@outlook.com>
2020-04-17 00:56:16 +0800

    feat(virtualenv-plugin): Add conda env support

--- plugins/virtualenv.ps1
+++ plugins/virtualenv.ps1
@@ -1,20 +1,24 @@
-# Plugin to display virtualenv in prompt
+# Plugin to display Python virtualenv in prompt
 
 function pshazz:virtualenv:init {
-	$virtualenv = $global:pshazz.theme.virtualenv
+    $virtualenv = $global:pshazz.theme.virtualenv
 
-	$global:pshazz.virtualenv = @{
-		prompt_lbracket = $virtualenv.prompt_lbracket;
-		prompt_rbracket = $virtualenv.prompt_rbracket;
-	}
+    $global:pshazz.virtualenv = @{
+        prompt_lbracket = $virtualenv.prompt_lbracket;
+        prompt_rbracket = $virtualenv.prompt_rbracket;
+    }
 }
 
 function global:pshazz:virtualenv:prompt {
-	$vars = $global:pshazz.prompt_vars
+    $vars = $global:pshazz.prompt_vars
 
-    if($env:VIRTUAL_ENV) {
+    if ($env:VIRTUAL_ENV -or $env:CONDA_DEFAULT_ENV) {
         $vars.virtualenv_lbracket = $global:pshazz.virtualenv.prompt_lbracket
         $vars.virtualenv_rbracket = $global:pshazz.virtualenv.prompt_rbracket
-        $vars.virtualenv = (Get-Item $env:VIRTUAL_ENV).BaseName
+        if ($env:VIRTUAL_ENV) {
+            $vars.virtualenv = (Get-Item $env:VIRTUAL_ENV).BaseName
+        } else {
+            $vars.virtualenv = $env:CONDA_DEFAULT_ENV
+        }
     }
 }
