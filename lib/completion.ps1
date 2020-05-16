cb43061ae82f0f71cb540f360f3a810632ba77da
Chawye Hsu <h404bi@outlook.com>
2020-05-16 16:29:46 +0800

    fix(completion): Add existing tab expansion fallback
    This is a temporary workaround to make pshazz work with other modules which also hook TabExpansion.
    Manipulating global functions is not a good idea, I'll refactor the whole functionality in the future.
    
    Signed-off-by: Chawye Hsu <h404bi@outlook.com>

--- lib/completion.ps1
+++ lib/completion.ps1
@@ -1,10 +1,23 @@
-# tab completion
-function global:tabExpansion($line, $lastWord) {
-    $expression = [Regex]::Split($line, '[|;]')[-1].trimstart()
+# pshazz tab completion
+
+# Backup previous TabExpansion function
+if (Test-Path Function:\TabExpansion) {
+    Rename-Item Function:\TabExpansion global:PshazzTabExpansionBackup
+}
+
+# Override TabExpansion function
+# FIXME: DO NOT override global TabExpansion function
+function global:TabExpansion($line, $lastWord) {
+    $expression = [regex]::Split($line, '[|;]')[-1].TrimStart()
 
     foreach($cmd in $global:pshazz.completions.keys) {
         if ($expression -match "^$cmd\s+(?<fragment>.*)") {
             return & $global:pshazz.completions[$cmd] $matches['fragment']
         }
+    }
+
+    # Fall back on existing tab expansion
+    if (Test-Path Function:\PshazzTabExpansionBackup) {
+        PshazzTabExpansionBackup $line $lastWord
     }
 }
